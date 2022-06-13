2cf1785a3298247b9d14e6d8fdd8dea74fc4b9e1
Winniebran <winnie_brandao@yahoo.com.br>
2022-06-13 15:27:56 -0300

    primeira atividade da sprint 3

---
+++ Modulo 1/Sprint3/A1-praticando-variaveis.js
@@ -0,0 +1,70 @@
+//Exercício 1
+let nome = "Winnie"
+console.log (`Olá, eu sou a ${nome}`)
+
+//Exercício 2
+nome = "Dreic"
+let sobrenome = "Leal"
+console.log (`Meu namorado se chama ${nome} ${ sobrenome}`)
+
+//Exercício 3
+let primeiroNome = "Winnie"
+let segundoNome = "Brandão"
+let idade = 29
+console.log (`Olá, meu primeiro nome é ${primeiroNome}, meu segundo nome é ${segundoNome}. E tenho ${idade} anos.`)
+
+//Exercício 4
+let preco1 = 20
+let preco2 = 32
+let precoTotal = preco1 + preco2
+console.log (`Comprei uma xícara por ${preco1} reais, e dois copos por ${preco2} reais. No total, gastei ${precoTotal} reais.`)
+
+//Exercício 5
+let numero1 = 10
+let numero2 = 15
+let numeroTotal = numero1 + numero2
+console.log (`A soma do valor ${numero1} com o valor do ${numero2} é igual a ${numeroTotal}, utilizando as variáveis declaradas anteriormente.`)
+
+//Exercício 6
+let contador = 0
+let calculadora = contador + 1
+console.log (calculadora)
+contador = 1
+calculadora = contador + 1
+console.log (calculadora)
+contador = 2
+calculadora = contador + 1
+console.log (calculadora)
+contador = 3
+calculadora = contador + 1
+console.log (calculadora)
+contador = 4
+calculadora = contador + 1
+console.log (calculadora)
+
+//Exercício 7
+let anoAtual = 2022
+let anoNascimento = 1992
+let idadeAtual = anoAtual - anoNascimento
+console.log (`No ano ${anoAtual}, eu vou ter ${idadeAtual} anos de idade`)
+
+//Exercício 8
+let alturaRetangulo = 5
+let larguraRetangulo = 7
+let areaRetangulo = alturaRetangulo * alturaRetangulo
+console.log(`Um retângulo com largura de ${larguraRetangulo}cm e altura de ${alturaRetangulo}cm, possui ${areaRetangulo}cm² de área total.`)
+
+//Exercício 9
+let diaAtual = 13
+let mesAtual = 06
+anoAtual = 2022
+let dataAtualFormatada = (`${diaAtual}/${mesAtual}/${anoAtual}`)
+console.log(`A data de hoje é ${dataAtualFormatada}.`)
+
+//Exercício 10
+let valorA = 56
+let valorB = 36
+let AtualValorA = valorB
+let AtualValorB = valorA
+console.log(`Antes a variável ValorA possuía o valor ${valorA}, agora possui o valor ${AtualValorA}.`)
+console.log(`Antes a variável ValorB possuía o valor ${valorB}, agora possui o valor ${AtualValorB}.`)
