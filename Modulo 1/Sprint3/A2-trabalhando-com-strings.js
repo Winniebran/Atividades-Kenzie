453cb11a62fc5c94de2be0b41dea4d1df594f29e
Winniebran <winnie_brandao@yahoo.com.br>
2022-06-13 16:05:09 -0300

    segunda atividade

---
+++ Modulo 1/Sprint3/A2-trabalhando-com-strings.js
@@ -0,0 +1,18 @@
+//Exercício 1
+let meuNome = "Winnie"
+comprimentoMeuNome = meuNome.length
+console.log(`O meu nome possui ${comprimentoMeuNome} caracteres de comprimento.`)
+
+//Exercício 2
+let preco = 15
+let precoFormatado = "R$15,00"
+console.log(`O valor ${preco} formarado ficaria assim ${precoFormatado}.`)
+
+//Exercício 3
+let cumprimento = "Bom dia"
+let nome = "Winnie"
+let sobrenome = "Brandão"
+let mensagemCumprimento = (`${cumprimento}, ${ nome} ${ sobrenome}`)
+console.log(mensagemCumprimento)
+
+
