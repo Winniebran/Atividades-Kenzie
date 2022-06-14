65c6d2fd3b53916f760273b5d8f597472f2a568c
Winniebran <winnie_brandao@yahoo.com.br>
2022-06-14 11:49:04 -0300

    adição da atividade 3

---
+++ Modulo 1/Sprint3/A3-Javascript-comparações.js
@@ -0,0 +1,17 @@
+//Exercício 1
+let valor1 = 10
+let valor2 = "10"
+let comparacaoDeValores = valor1 === valor2
+console.log(comparacaoDeValores)
+
+//Exercício 2
+let usuario1Altura = 1.80
+let usuario2Altura = 1.75
+let comparacaoDeAlturas = usuario1Altura > usuario2Altura
+console.log(comparacaoDeAlturas)
+
+//Exercício 3
+valor1 = 10
+valor2 = "10"
+comparacaoDeValores = valor1 !== valor2
+console.log(comparacaoDeValores)
