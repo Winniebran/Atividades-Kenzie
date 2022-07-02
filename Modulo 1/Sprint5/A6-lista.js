3c5e3fed9c529a76d87c4db0971184a0ed7aff4b
Winniebran <winnie_brandao@yahoo.com.br>
2022-07-02 12:46:52 -0300

    Atividade 6, S5

---
+++ Modulo 1/Sprint5/A6-lista.js
@@ -0,0 +1,17 @@
+let list = "Verifcar se está conectado na energia, Verificar se o monitor está conectado na energia, Verificar se todos os cabos estão conectados, Apertar o botão de power, Esperar tela acender, usar"
+
+function convertToArray(str){
+    let strToArr = str.split(',')
+    return strToArr
+}
+console.log(convertToArray(list))
+
+function convertToArray2(arr){
+    let arrToList = []
+    for ( i = 1; i < arr.length; i++){
+        let newArr = `${i}º -${arr[i]}`
+        arrToList.push(newArr)
+    }
+    return arrToList
+}
+console.log(convertToArray2(convertToArray(list)))
