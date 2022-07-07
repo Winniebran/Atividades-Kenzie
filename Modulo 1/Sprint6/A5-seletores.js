fda04e492891da505c8deacfcaf21474d27a538e
Winniebran <winnie_brandao@yahoo.com.br>
2022-07-07 12:12:06 -0300

    Atividade de seletores, S6

---
+++ Modulo 1/Sprint6/A5-seletores.js
@@ -0,0 +1,11 @@
+//Simples
+const seletor = document.querySelector('h1')
+console.log(seletor)
+
+//Intermediario
+const seletor1 = document.getElementById('title')
+console.log(seletor1)
+
+//avançado
+const seletor2 = document.querySelector('.titles')
+console.log(seletor2)
