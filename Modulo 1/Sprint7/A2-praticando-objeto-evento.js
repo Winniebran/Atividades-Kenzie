4329f9331a234941adf9e6acce4b50225594e4be
Winniebran <winnie_brandao@yahoo.com.br>
2022-07-14 20:44:32 -0300

    Atividade 2, S7 - 80% pronta

--- Modulo 1/Sprint7/A2-praticando-objeto-evento.js
+++ Modulo 1/Sprint7/A2-praticando-objeto-evento.js
@@ -5,6 +5,8 @@ let produto = {
     distribuidor: 'lojadakenzie.com.br'
 }
 
+const body = document.querySelector('body')
+
 function newEvent(object){
 
     let tagSpan1 = document.createElement('span')
@@ -13,30 +15,58 @@ function newEvent(object){
     let tagSpan4 = document.createElement('span')
     let tagAncora = document.createElement ('a')
     let tagLi = document.createElement('li')
+    let tagUL = document.createElement('ul')
+    let valorComDesconto = object.valor - object.valor * 0.3
 
-    tagSpan1.innerText = `${object.nome}`
-    tagSpan2.innerText = `${object.valor}`
-    tagSpan3.innerText = `${object.liquidacao}`
-    tagSpan4.innerText =  ''
-    tagAncora.innerText = `${object.distribuidor}`
+    tagSpan1.innerText = `${object.nome}\n`
+    tagSpan2.innerText = `${object.valor}\n`
+    tagSpan3.innerText = `Liquidação\n`
+    tagAncora.innerText = `${object.distribuidor}\n`
+    tagAncora.href = 'https://alunos2.kenzie.com.br/courses/99/pages/s7-04%7C-atividade-praticando-objeto-evento'
+        
+    tagLi.append(tagSpan1, tagSpan2, tagSpan3, tagAncora, tagSpan4)
+    tagUL.appendChild(tagLi)
+    body.appendChild(tagUL)
+
+    tagAncora.addEventListener('click', function(a){
+      if (object.liquidacao === true){
+        a.preventDefault()
+      }  
+    })
+
+    tagSpan3.addEventListener('click', function(){
+      if(object.liquidacao === true){
+        tagSpan4.innerText = `Apenas hoje: R$ ${valorComDesconto}`
+      }else{
+        tagSpan4.innerText = ''
+      }
+
+      tagSpan3.addEventListener('click', function(){
+        if(tagSpan4.innerText == `Apenas hoje: R$ ${valorComDesconto}`){
+          tagSpan4.innerText = ''
+        }else{
+          tagSpan4.innerText = `Apenas hoje: R$ ${valorComDesconto}`
+        }
+      })
+    })
     
-    tagSpan4.appendChild(tagAncora)
-    tagLi.append(tagSpan1, tagSpan2, tagSpan3, tagSpan4)
-    
-    return tagLi
 }
-console.log(newEvent(produto))
+newEvent(produto)
 
-let element = document.getElementsByClassName("container")[0];
-let elementInformado = document.getElementsByClassName("infomacao")[0];
+
+
+
+/* let element = document.querySelector(".container")
+let elementInformado = document.querySelector(".informacao")
 
 element.addEventListener("click", function (e) {
     let elementoCapture = e.target.className;
+
     if (elementoCapture == "aqui_dispara") {
         if (elementInformado.children.length === 0) {
-          elementInformado.innerHTML = "Sou um toggle";
+          elementInformado.innerHTML = '<span>Sou um toggle</span>';
         } else {
           elementInformado.innerHTML = "";
         }
     }
-})
+}) */
