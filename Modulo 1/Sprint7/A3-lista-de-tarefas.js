a628685b24affa9190f11071642e2711c0b8c1c1
Winniebran <winnie_brandao@yahoo.com.br>
2022-07-15 15:52:54 -0300

    A3, S7 - JS finalizado

--- Modulo 1/Sprint7/A3-lista-de-tarefas.js
+++ Modulo 1/Sprint7/A3-lista-de-tarefas.js
@@ -18,6 +18,37 @@ let tasks = [
     {
       titulo: "Beber água",
       tipo: "prioritário"
-    }    
+    },
 ]
 
+const tarefas = document.querySelector('.tarefas')
+let createUl = document.createElement('ul')
+
+let arrUrgente = []
+let arrPrioritario = []
+let arrNormal = []
+
+function priorityTask(priority) {
+  for(let i = 0; i < priority.length; i++){
+    if(priority[i].tipo.toLowerCase() === 'urgente'){
+      arrUrgente.unshift(priority[i].titulo)
+    }else if(priority[i].tipo.replace('á', 'a').toLowerCase() === 'prioritario'){
+      arrPrioritario.unshift(priority[i].titulo)
+    }else if(priority[i].tipo.toLowerCase() === 'normal'){
+      arrNormal.unshift(priority[i].titulo)
+    }
+  }
+}
+priorityTask(tasks)
+
+function createTask(task){
+  for (let i = 0; i < task.length; i++){
+    let createLi = document.createElement('li')
+    createLi.innerText = `${task[i]}`
+    createUl.appendChild(createLi)
+    tarefas.appendChild(createUl)
+  }
+}
+createTask(arrUrgente)
+createTask (arrPrioritario)
+createTask (arrNormal)
